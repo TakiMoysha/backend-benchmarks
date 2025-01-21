ed4aac32223b6d30812b2fae45e3fd264bd1ec35
TakiMoysha <mpwema782@gmail.com>
2025-01-21 02:29:19 +0100

    updated arobyn

--- src/arobyn/cli.py
+++ src/arobyn/cli.py
@@ -7,9 +7,9 @@ def run_app():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-H", "--host", dest="host", default="0.0.0.0")
-    parser.add_argument("-p", "--port", dest="port", type=int, default=8080)
+    parser.add_argument("-p", "--port", dest="port", type=int, default=8000)
     parser.add_argument("-t", "--threads", dest="thr", type=int, default=1)
-    parser.add_argument("-w", "--workers", dest="wrk", type=int, default=4)
+    parser.add_argument("-w", "--workers", dest="wrk", type=int, default=1)
 
     required, extra = parser.parse_known_args(sys.argv[1:])
 
